ae756fae555a9a984ee8e33c339ff3721d660d2e
SaltyUA <00cmb.ua@gmail.com>
2023-08-02 14:37:03 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { fetchBreeds } from './js/cat-api';
 import { fetchCatByBreed } from './js/cat-api';
 import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
 
 const elements = {
   catInfo: document.querySelector(`.cat-info`),
@@ -8,34 +9,50 @@ const elements = {
   loader: document.querySelector(`.loader`),
   error: document.querySelector(`.error`),
 };
+
 elements.select.classList.add(`isHidden`);
 
-fetchBreeds().then(elements.select.classList.remove(`isHidden`));
+fetchBreeds()
+  .then(() => {
+    elements.loader.classList.add(`isHidden`);
+    elements.select.classList.remove(`isHidden`);
+  })
+  .catch(error => {
+    console.warn(error);
+    elements.loader.classList.add(`isHidden`);
+    Notiflix.Report.failure(
+      `Oops! Something went wrong! Try reloading the page!`
+    );
+  });
 
 elements.select.addEventListener('change', setHtml);
 
 function setHtml() {
+  elements.catInfo.innerHTML = ``;
   elements.loader.classList.remove(`isHidden`);
   fetchCatByBreed(elements.select.value)
     .then(cat => {
       elements.loader.classList.add(`isHidden`);
 
-      elements.catInfo.innerHTML = ``;
       let markup;
       cat.map(breed => {
         markup = `<img src="${breed.url}">
       `;
         breed.breeds.map(breed => {
-          markup += `<p class="breed-name">Breed Name: ${breed.name}</p>
-          <p class="breed-desc">Description: ${breed.description}</p>
-          <p class="breed-temp">Temperament: ${breed.temperament}</p>`;
+          markup += `<div class="breed-info">
+          <p class="breed-name"><b>Breed Name:</b> ${breed.name}</p>
+          <p class="breed-desc"><b>Description:</b> ${breed.description}</p>
+          <p class="breed-temp"><b>Temperament:</b> ${breed.temperament}</p>
+          </div>`;
         });
       });
       elements.catInfo.insertAdjacentHTML(`beforeend`, markup);
     })
-    .catch(error =>
+    .catch(error => {
+      console.warn(error);
+      elements.loader.classList.add(`isHidden`);
       Notiflix.Report.failure(
         `Oops! Something went wrong! Try reloading the page!`
-      )
-    );
+      );
+    });
 }
